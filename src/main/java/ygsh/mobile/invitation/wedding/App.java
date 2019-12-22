7d5f4773b812a02ab0fb1816b7f8515a7602b7ec
yg <yg@localhost>
2019-12-22 17:43:56 +0900

    created project

---
+++ src/main/java/ygsh/mobile/invitation/wedding/App.java
@@ -0,0 +1,13 @@
+package ygsh.mobile.invitation.wedding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
